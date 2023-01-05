a7cb8479a5ba318bad4c72d92adfed95a661d09d
dawidperla <108357006+dawidperla@users.noreply.github.com>
2023-01-05 06:27:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from notification_manager import NotificationManager
 
 ORIGIN_CITY_IATA = "LON"
 
+
 data_manager = DataManager()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
 
+
 sheet_data = data_manager.get_destination_data()
+
 
 if sheet_data[0]["iataCode"] == "":
     city_names = [row["city"] for row in sheet_data]
     data_manager.city_codes = flight_search.get_destination_codes(city_names)
     data_manager.update_destination_codes()
     sheet_data = data_manager.get_destination_data()
+    
 
 destinations = {
     data["iataCode"]: {
@@ -25,8 +29,10 @@ destinations = {
         "price": data["lowestPrice"]
     } for data in sheet_data}
 
+
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=6 * 30)
+
 
 for destination_code in destinations:
     flight = flight_search.check_flights(
@@ -37,14 +43,17 @@ for destination_code in destinations:
     )
     if flight is None:
         continue
+        
 
     if flight.price < destinations[destination_code]["price"]:
         message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        
 
         ######################
         if flight.stop_overs > 0:
             message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
             print(message)
         #######################
+        
 
         notification_manager.send_sms(message)
