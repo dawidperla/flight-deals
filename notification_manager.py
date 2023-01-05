ed1b3f951eef015ce7f5653e055ff3bfce5073e1
dawidperla <108357006+dawidperla@users.noreply.github.com>
2023-01-05 06:30:56 +0100

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 
+
 TWILIO_SID = "AC3de603666fdbf7f13a134fdc8cdfa1f8"
 TWILIO_AUTH_TOKEN = "5e207d6c85e90ea73b72c665525797c8"
 TWILIO_VIRTUAL_NUMBER = "+13192545649"
@@ -8,9 +9,11 @@ TWILIO_VERIFIED_NUMBER = "+48698117913"
 
 class NotificationManager:
 
+    
     def __init__(self):
         self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
+        
     def send_sms(self, message):
         message = self.client.messages.create(
             body=message,
